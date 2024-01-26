b9f2b55438a182d4327a73bce233ecae48b02f61
Sankalp Raj <100083553+sankalpraj424@users.noreply.github.com>
2024-01-26 16:22:38 +0530

    Add files via upload

---
+++ src/store.js
@@ -0,0 +1,11 @@
+// store.js
+
+import { createStore, applyMiddleware } from 'redux';
+import {thunk} from 'redux-thunk';
+import rootReducer from './reducers';
+
+const storedFields = JSON.parse(localStorage.getItem('fields')) || [];
+
+const store = createStore(rootReducer, { fields: { fields: storedFields } }, applyMiddleware(thunk));
+
+export default store;
